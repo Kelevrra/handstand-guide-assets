6e9a95bb5c93a0db15be43b3aad386586a377316
Alex <thunderalexra@gmail.com>
2025-07-20 20:38:50 +0200

    Crow add

---
+++ src/components/TrainStrengthToPrepare.tsx
@@ -0,0 +1,16 @@
+import {Box, Flex, Title} from "@mantine/core";
+import React from "react";
+import {VideoBox} from "./VideoBox/VideoBox";
+
+export const TrainStrengthToPrepare = () => {
+  return <Box mt={32} maw={500} ml="auto" mr="auto">
+    <Title>Train Strength</Title>
+    <Box mt={32}>
+      <VideoBox path="/TrainStrengthToPrepare/crow.mp4" label="crow" />
+      <VideoBox path="/TrainStrengthToPrepare/low-crow.mp4" label="low-crow" />
+      <VideoBox path="/TrainStrengthToPrepare/left-crow.mp4" label="left-crow" />
+      <VideoBox path="/TrainStrengthToPrepare/right-crow.mp4" label="right-crow" />
+      <VideoBox path="/TrainStrengthToPrepare/crow-switch.mp4" label="crow-switch" />
+    </Box>
+  </Box>
+}
---
+++ src/components/VideoBox/VideoBox.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from "react";
+import {Box} from "@mantine/core";
+
+export const VideoBox: FC<{ path: string; label: string }> = ({path, label}) => {
+  return <Box mt={32} mb={32}>
+    <Box fz={16}>{label}</Box>
+    <video className="video" autoPlay loop muted playsInline>
+      <source src={path} type="video/mp4"/>
+      Your browser does not support the video tag.
+    </video>
+  </Box>
+}
