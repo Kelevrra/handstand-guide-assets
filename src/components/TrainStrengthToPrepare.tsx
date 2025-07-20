7acc1e4c44b02c2b9b10f6e9446c6cf8723f43b3
Alex <thunderalexra@gmail.com>
2025-07-20 21:31:19 +0200

    TrainStrengthToPrepare

--- src/components/TrainStrengthToPrepare.tsx
+++ src/components/TrainStrengthToPrepare.tsx
@@ -6,6 +6,8 @@ export const TrainStrengthToPrepare = () => {
   return <Box mt={32} maw={500} ml="auto" mr="auto">
     <Title>Train Strength</Title>
     <Box mt={32}>
+      <Title order={2}>Other</Title>
+
       <VideoBox path="/TrainStrengthToPrepare/crow.mp4" label="crow" />
       <VideoBox path="/TrainStrengthToPrepare/low-crow.mp4" label="low-crow" />
       <VideoBox path="/TrainStrengthToPrepare/left-crow.mp4" label="left-crow" />
