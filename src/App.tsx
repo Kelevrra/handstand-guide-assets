a5333d4c8a21fa5021c5bb3ac0a94e07f85ff40a
Alex <thunderalexra@gmail.com>
2025-07-20 21:44:59 +0200

    Bg

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import {MiddleWorkout} from "./components/MiddleWorkout";
 import {SeniorWorkout} from "./components/SeniorWorkout";
 import {OtherWorkout} from "./components/OtherWorkout";
 import {Wheel} from "./components/Wheel";
+import {VideoBox} from "./components/VideoBox/VideoBox";
 
 function App() {
   return (
     <div className="App">
-      <TrainStrengthToPrepare/>
 
+      <VideoBox path="/bg_1_cut.mp4" label="bg_1_cut" />
+      <VideoBox path="/cover_video.mp4" label="cover_video" />
+
+      <TrainStrengthToPrepare/>
       <JuniorWorkout/>
       <MiddleWorkout/>
       <SeniorWorkout/>
       <OtherWorkout/>
-<Wheel />
+      <Wheel />
     </div>
   );
 }
