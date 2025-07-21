a73219376a6928f3667e4b508ca1c244ee6c77bf
Alex <thunderalexra@gmail.com>
2025-07-21 14:00:42 +0200

    Warmup

---
+++ src/components/Warmup.tsx
@@ -0,0 +1,44 @@
+import {Box, Title} from "@mantine/core";
+import React from "react";
+import {VideoBox} from "./VideoBox/VideoBox";
+
+export const Warmup = () => {
+  return <Box mt={32} maw={500} ml="auto" mr="auto">
+    <Title order={2}>Warmup</Title>
+    <Box mt={32}>
+      <VideoBox path="/Warmup/core_side_leans.mp4" label="core_side_leans" />
+      <VideoBox path="/Warmup/core_front_leans_hold.mp4" label="core_front_leans_hold" />
+      <VideoBox path="/Warmup/jump_jack.mp4" label="jump_jack" />
+      <VideoBox path="/Warmup/core_hip_rotations.mp4" label="core_hip_rotations" />
+      <VideoBox path="/Warmup/core_front_leans_squots.mp4" label="core_front_leans_squots" />
+      <VideoBox path="/Warmup/core_front_leans.mp4" label="core_front_leans" />
+      <VideoBox path="/Warmup/core_rotations.mp4" label="core_rotations" />
+      <VideoBox path="/Warmup/back_rotations_elbow.mp4" label="back_rotations_elbow" />
+      <VideoBox path="/Warmup/front_rotations_elbow.mp4" label="front_rotations_elbow" />
+      <VideoBox path="/Warmup/legs_and_pelvis_knee_rotation.mp4" label="legs_and_pelvis_knee_rotation" />
+      <VideoBox path="/Warmup/legs_and_pelvis_calf_raises.mp4" label="legs_and_pelvis_calf_raises" />
+      <VideoBox path="/Warmup/legs_and_pelvis_knee_kick_rotations.mp4" label="legs_and_pelvis_knee_kick_rotations" />
+      <VideoBox path="/Warmup/legs_and_pelvis_fit_rotations.mp4" label="legs_and_pelvis_fit_rotations" />
+      <VideoBox path="/Warmup/mobility_goddess_pose.mp4" label="mobility_goddess_pose" />
+      <VideoBox path="/Warmup/mobility_stabilisation.mp4" label="mobility_stabilisation" />
+      <VideoBox path="/Warmup/mobility_front_lean_hold.mp4" label="mobility_front_lean_hold" />
+      <VideoBox path="/Warmup/mobility_skandasana_archer.mp4" label="mobility_skandasana_archer" />
+      <VideoBox path="/Warmup/mobility_leans_to_plank_stretch.mp4" label="mobility_leans_to_plank_stretch" />
+      <VideoBox path="/Warmup/neck_side_rotations.mp4" label="neck_side_rotations" />
+      <VideoBox path="/Warmup/neck_rotations.mp4" label="neck_rotations" />
+      <VideoBox path="/Warmup/neck_side_lean.mp4" label="neck_side_lean" />
+      <VideoBox path="/Warmup/neck_front_inclines.mp4" label="neck_front_inclines" />
+      <VideoBox path="/Warmup/rubbing_871b63f5d1.mp4" label="rubbing_871b63f5d1" />
+      <VideoBox path="/Warmup/shoulders_shoulders_back.mp4" label="shoulders_shoulders_back" />
+      <VideoBox path="/Warmup/shoulders_shoulders_front.mp4" label="shoulders_shoulders_front" />
+      <VideoBox path="/Warmup/shoulders_sisors.mp4" label="shoulders_sisors" />
+      <VideoBox path="/Warmup/shoulders_rotations_back_7d72dcb3ba.mp4" label="shoulders_rotations_back_7d72dcb3ba" />
+      <VideoBox path="/Warmup/shoulders_rotations.mp4" label="shoulders_rotations" />
+      <VideoBox path="/Warmup/wrist_plank_rotatons.mp4" label="wrist_plank_rotatons" />
+      <VideoBox path="/Warmup/wrists_plank_raises.mp4" label="wrists_plank_raises" />
+      <VideoBox path="/Warmup/wrist_rotations.mp4" label="wrist_rotations" />
+      <VideoBox path="/Warmup/wrists_plank_steetch.mp4" label="wrists_plank_steetch" />
+      <VideoBox path="/Warmup/push_up.mp4" label="push_up" />
+    </Box>
+  </Box>
+} 
